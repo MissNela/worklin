38c6d576d940ad8f13284b68d33d7a2c09db8fe5
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-24 18:45:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,10 +46,7 @@ async def warn(ctx, userName: discord.User, *, message:str):
 async def ban(ctx,user:discord.Member):
    channel = discord.utils.get(client.get_all_channels(), name='warn-logs')
 
-   if user.server_permissions.ban_members:
-        await client.say('**He is mod/admin and i am unable to ban him/her**')
-        return
-
+   
     try:
         await client.ban(user)
         await client.send_message(channel, """
