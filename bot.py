c2a4f850eb3af9fd5ef1b8ffdd2df2e6d73f2525
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-24 16:30:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def warn(ctx, userName: discord.User, *, message:str):
     __**{0}**__
     **Z dúvodu:
     {1}** """.format(userName,message))
-    await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+    await client.send_message(channel, embed=discord.Embed(color=color)
 
 
 
