4f20bd586aa8968acd3ab34c7c8832114aaf6617
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-24 16:42:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,10 +38,9 @@ async def warn(ctx, userName: discord.User, *, message:str):
     await client.say(channel, """
     :warning: Varování pro:
     __**{0}**__
-    **Z dúvodu:
-    {1}** """)
-    await client.send_message(channel, embed=discord.Embed(color=color))
-
+    Z dúvodu:
+    **__{1}__** """)
+    
 
 
 client.run(os.getenv("BOT_TOKEN"))
