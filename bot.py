510079330e0a6208d95c6d28569e57294cfd0ed4
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-24 14:04:57 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import platform
+import colorsys
+import random
+import os
+import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
+
+
+
+
+
+
+
+client = commands.Bot(command_prefix = '/')
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name= "Prefix: /, Dev Nela"))
+    print("The bot is online and connected with Discord!")
+
+
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
+
+async def warn(ctx, userName: discord.User, *, message:str):
+        await client.send_message(userName, "You have been warned for: {}".format(message)) 
+        await client.say("warning {0} Has Been Warned! Warning Reason : {1} ".format(userName,message))
+        pass
+
+client.run(os.getenv("BOT_TOKEN"))
