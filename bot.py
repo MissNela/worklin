242b89b0abbb35359eb0d2eb3143ce6ccd8c0c8b
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-24 14:24:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,19 +19,25 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 client = commands.Bot(command_prefix = '/')
 
+
+
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name= "Prefix: /, Dev Nela"))
     print("The bot is online and connected with Discord!")
 
 
-
-@client.command(pass_context=True)
+@client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)
+async def warn(ctx, userName: discord.User, *, message:str): 
+    await client.send_message(userName, "Byl jsi varován za: **{}**".format(message))
+    await client.say("""
+    :warning: Varování pro:
+    __**{0}**__
+    **Z dúvodu:
+    {1}** """.format(userName,message))
+    pass
 
-async def warn(ctx, userName: discord.User, *, message:str):
-        await client.send_message(userName, "You have been warned for: {}".format(message)) 
-        await client.say("warning {0} Has Been Warned! Warning Reason : {1} ".format(userName,message))
-        pass
+
 
 client.run(os.getenv("BOT_TOKEN"))
